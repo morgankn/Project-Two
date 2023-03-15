a278e192d35d2ae606cb08e4dac4349eb8126a6f
LearnedDr <fredrickj.chang@gmail.com>
2023-03-14 22:22:36 -0400

    commented out htmlroutes index to have it working for now

--- controllers/htmlRoutes/index.js
+++ controllers/htmlRoutes/index.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+// const axios = require('axios');
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
@@ -10,25 +10,23 @@ router.get('/', async (req, res) => {
 router.get('/login', async (req, res) => {
   res.render('login');
 });
-router.get('/search/:searchFlight', authChecker, async (req, res) => {
-  const { searchTerm } = req.params;
-  const origin = doc.queryselector(userinput);
-  const destination = doc.queryselector(userinput);
-  const departureDate = doc.req.params
-    await axios.get(`https://app.goflightlabs.com/search-all-flights?access_key={process.env.API_KEY}&adults=1&${origin.val}&${destination.val}&${departureDate.val}&${currency.val}&${arriveTo.val}
+// router.get('/search/:searchFlight', authChecker, async (req, res) => {
+//   const { searchTerm } = req.params;
+//   const origin = doc.queryselector(userinput);
+//   const destination = doc.queryselector(userinput);
+//   const departureDate = doc.req.params;
+//   await axios.get(`https://app.goflightlabs.com/search-all-flights?access_key={process.env.API_KEY}&adults=1&${origin.val}&${destination.val}&${departureDate.val}&${currency.val}&${arriveTo.val}
 
-    `);
+//     `);
 
-  const flight = flightResponse.data.items.map((flight) => ({
-    id: flight.id,
+//   const flight = flightResponse.data.items.map((flight) => ({
+//     id: flight.id,
+//   }));
 
-  
-  }));
-
-  res.render('search', {
-    flight,
-  });
-});
+//   res.render('search', {
+//     flight,
+//   });
+// });
 
 router.get('/logout', async (req, res) => {
   req.session.destroy();
